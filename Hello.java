1c45f65faee32150724555792d1500ad116494c0
sanjananelluri <sanjananelluri1@gmail.com>
2024-10-03 14:52:27 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,7 @@
+// HelloWorld.java
+public class Hello{
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
